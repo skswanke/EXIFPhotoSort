04846c988bb99bf7f1d61e98ce433c6847fb554d
Sam Swanke <skswanke@users.noreply.github.com>
2014-11-05 23:34:36 -0500

    exif photo sort

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileDialogButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(288, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sort your pictures by date Taken!";
+            // 
+            // fileDialogButton
+            // 
+            this.fileDialogButton.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileDialogButton.Location = new System.Drawing.Point(12, 62);
+            this.fileDialogButton.Name = "fileDialogButton";
+            this.fileDialogButton.Size = new System.Drawing.Size(118, 46);
+            this.fileDialogButton.TabIndex = 4;
+            this.fileDialogButton.Text = "Select Folder";
+            this.fileDialogButton.UseVisualStyleBackColor = true;
+            this.fileDialogButton.Click += new System.EventHandler(this.fileDialogButton_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Pick the folder that you would like to sort.";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected folder:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.progressBar1.Location = new System.Drawing.Point(136, 121);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(294, 17);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(136, 142);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Copy Files instead of Move";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 168);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileDialogButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Sort Pictures by Date";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button fileDialogButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string[] validExtentions = new string[4] {".jpg", ".JPG", ".jpeg", ".JPEG"};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fileSkipCount = 0;
+            bool errors = false;
+            button1.Enabled = false;
+            fileDialogButton.Enabled = false;
+            bool copyFiles = checkBox1.Checked;
+            if (this.textBox1.Text == ""||this.textBox1.Text.Contains("Files"))
+            {
+                MessageBox.Show("You need to Select a file first!", "Message");
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(this.textBox1.Text);
+                progressBar1.Maximum = files.Length;
+
+                try
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        int propNum = 0;
+                        bool validProp = false;
+                        ASCIIEncoding encoding = new ASCIIEncoding();
+                        progressBar1.Value += 1;
+                        
+                        try 
+                        {
+                            if (validExtentions.Contains(Path.GetExtension(files[i].ToString())))
+                            {
+                                //Load image
+                                Image theImage = new Bitmap(@files[i].ToString());
+                                Console.WriteLine("Image Loaded");
+                                Console.WriteLine(files[i].ToString());
+
+                                //Get Exif
+                                PropertyItem[] propItems = theImage.PropertyItems;
+                                Console.WriteLine("Property Items Found");
+
+                                //Clear Ref
+                                theImage.Dispose();
+                                Console.WriteLine("Image Ref Disposed");
+                         
+                                //Validate Properties
+                                while (!validProp && propNum < 15)
+                                {
+                                    string dateTaken = encoding.GetString(propItems[propNum].Value);
+                                    
+                                    //Check Items
+                                    if (propItems.Length == 0)
+                                    {
+                                        errors = true;
+                                        fileSkipCount += 1;
+                                        Console.WriteLine("propItems Length = 0");
+                                        validProp = true;
+                                    }
+
+                                    //Check that the items are there
+                                    else if (propItems.Length < 7)
+                                    {
+                                        errors = true;
+                                        fileSkipCount += 1;
+                                        Console.WriteLine("propItems Length < 7");
+                                        validProp = true;
+                                    }
+
+                                    //Check that the desired prop is there
+                                    else if (propItems[propNum] == null || dateTaken.Length > 30)
+                                    {
+                                        Console.WriteLine("propItems[" + propNum + "] was null");
+                                        propNum += 1;
+                                    }
+
+                                    //If it is make sure it is in calender format
+                                    else if (!dateTaken.Contains(':'))
+                                    {
+                                        Console.WriteLine("propItems[" + propNum + "] was not a date");
+                                        propNum += 1;
+                                    }
+
+                                    //Sort into new folders
+                                    else
+                                    {
+                                        Console.WriteLine("DateTaken was Found!");            
+                                        string originPath = files[i].ToString();
+                                        string newPath = this.textBox1.Text + '\\' + dateTaken.Substring(0, 4) + '_' + dateTaken.Substring(5, 2) + '\\' + Path.GetFileName(files[i].ToString());
+                                        string newFolder = this.textBox1.Text + '\\' + dateTaken.Substring(0, 4) + '_' + dateTaken.Substring(5, 2) + '\\';
+                                        if (!Directory.Exists(newFolder))
+                                        {
+                                            Directory.CreateDirectory(newFolder);
+                                            Console.WriteLine("New Directory Was Created");
+                                        }
+
+                                        //Move or Copy Files
+                                        if (!checkBox1.Checked)
+                                        {
+                                            File.Move(files[i].ToString(), this.textBox1.Text + '\\' + dateTaken.Substring(0, 4) + '_' + dateTaken.Substring(5, 2) + '\\' + Path.GetFileName(files[i].ToString()));
+                                            Console.WriteLine("Files were Moved");
+                                        }
+                                        else
+                                        {
+                                            File.Copy(files[i].ToString(), this.textBox1.Text + '\\' + dateTaken.Substring(0, 4) + '_' + dateTaken.Substring(5, 2) + '\\' + Path.GetFileName(files[i].ToString()));
+                                            Console.WriteLine("Files were Copied");
+                                        }
+                                        validProp = true;
+                                    }
+                                }
+                                //Make sure fileskip count counts files that went through 15 checks
+                                if (!validProp)
+                                    fileSkipCount += 1;
+                            }
+
+                            //If the extention was not Valid
+                            else
+                            {
+                                Console.WriteLine("A file was Skipped");
+                                errors = true;
+                                fileSkipCount += 1;
+                                validProp = true;
+                            }
+                        }
+
+                        //General Catch
+                        catch (Exception)
+                        {
+                            Console.WriteLine("File Skipped Due to Error");
+                            fileSkipCount += 1;
+                            continue;
+                        }
+                    }
+                    //Tell the user if there were errors
+                    if (errors)
+                    {
+                        MessageBox.Show("Items were skipped! (no Exif)\nFiles Skipped: " + fileSkipCount, "Message");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Success!", "Message");
+                    }
+                    this.textBox1.Text = "Files Skipped: " + fileSkipCount.ToString();
+                }
+                
+                catch (Exception)
+                {
+                    MessageBox.Show("Error", "Error");
+                    throw;
+                }
+            }
+            button1.Enabled = true;
+            fileDialogButton.Enabled = true;
+            progressBar1.Value = 0;
+        }
+
+        private void fileDialogButton_Click(object sender, EventArgs e)
+        {
+            DialogResult folderLocation = folderBrowserDialog1.ShowDialog();
+            if (folderLocation == DialogResult.OK)
+            {
+                string[] files1 = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                MessageBox.Show("Files Found: " + files1.Length.ToString(), "Message");
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+            Console.WriteLine(folderLocation);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
